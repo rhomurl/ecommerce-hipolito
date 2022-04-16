adf9afdb7a271cdbf2719630d1ad6d5cef48d083
R M <31006518+ramrop@users.noreply.github.com>
2022-04-17 01:56:56 +0800

    Remove file and updated users table

--- database/migrations/2022_03_07_115827_add_address_book_id_to_users_table.php
+++ database/migrations/2022_03_07_115827_add_address_book_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddAddressBookIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('address_book_id');
+            $table->integer('address_book_id')->nullable();
         });
     }
 
