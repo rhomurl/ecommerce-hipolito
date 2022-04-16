df7a8df9485478388a270b2d548d2a56c4fd218f
R M <31006518+ramrop@users.noreply.github.com>
2022-04-16 22:32:51 +0800

    Tailwind css internal

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
         <title>@yield('title') - Admin</title>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         
-        <link rel="stylesheet" href="{{ asset('dashboard/build/tailwind.css') }}" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
         <script src="{{ asset('dashboard/assets/js/init-alpine.js') }}"></script>
 
