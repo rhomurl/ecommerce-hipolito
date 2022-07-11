ee8c597ac2ec55271e655e26fa942a837cf3ef34
Rhomuel Brian Macahilig <31006518+re-dump@users.noreply.github.com>
2022-07-11 14:11:13 +0800

    Modified sales report group

--- app/Http/Livewire/Admin/SalesReportGroup.php
+++ app/Http/Livewire/Admin/SalesReportGroup.php
@@ -6,13 +6,14 @@ use App\Models\{Order, OrderProduct};
 use Carbon\Carbon;
 use DB;
 use Livewire\Component;
+use PDF;
 use Session;
 
 class SalesReportGroup extends Component
 {
     public $selected_filter, $group_by;
     public $month_from, $month_to, $year_from, $year_to;
-    public $date_from, $date_to;
+    public $date_from, $date_to, $genOrders, $range;
 
     public function mount(){
         
@@ -33,19 +34,88 @@ class SalesReportGroup extends Component
         }
     }
 
+    public function generatePDF(){
+        switch($this->selected_filter) {
+            case('date'):
+
+                if($this->date_from && $this->date_to){
+                   $range = $this->date_from . "_to_" . $this->date_to;
+                }
+                else if($this->date_from){
+                    $range = $this->date_from;
+                }
+                break;
+ 
+            case('month'):
+
+                if($this->month_from && $this->year_from && $this->year_to && $this->year_from)
+                {
+                    $range = $this->month_from . "_" . $this->year_from . "_to_" . $this->month_to . "_" . $this->year_to;
+                }
+                else if($this->month_from && $this->year_from){
+                    $range = $this->month_from . "_" . $this->year_from;
+                }
+                
+                break;
+ 
+            case('year'):
+
+                if($this->year_from && $this->year_to)
+                {
+                    $range = $this->year_from . "_to_" . $this->year_to;
+                }
+                else if($this->year_from)
+                {
+                    $range = $this->year_from; 
+                }
+                   
+                break;
+            default:
+                $range = "all";
+        }
+
+        $data = [
+            'genOrders' => $this->genOrders,
+            'generatedAt' => Carbon::now()->format('M d Y h:i A'),
+            'range' => $range,
+            'group_by' => $this->group_by
+        ];
+
+        //dd($data);
+        //$pdf = PDF::loadView('pdf.sales-report', $data);
+        //return $pdf->download('sales-report_.pdf');
+        //return $pdf->stream();
+
+        $pdfContent = PDF::loadView('pdf.sales-report-group', $data)->output();
+        return response()->streamDownload(
+            fn () => print($pdfContent),
+            "sales_report_".$range.".pdf"
+        );
+    }
+
     public function redirectTo($route){
         return redirect()->route($route);
     }
 
     public function render()
     {
-        //$orders = Order::select("order_product.*", DB::raw("(sum(total)) as total_order"), DB::raw("(DATE_FORMAT(created_at, '%m-%Y')) as month_year"));
-        $cart = OrderProduct::select("order_product.*", DB::raw("sum(quantity) as product_qty"))
-        ->groupBy('order_product.order_id', 'order_product.product_id')
-        
-        ->get();
-        dd($cart);
-
+        /*
+        "id" => 2
+        "user_id" => 2
+        "address_book_id" => 1
+        "subtotal" => 1610.0
+        "discount" => null
+        "shippingfee" => 300.0
+        "total" => 9000.0
+        "status" => "ordered"
+        "created_at" => "2022-10-29 20:54:05"
+        "updated_at" => "2022-07-09 20:54:05"
+        "transaction_id" => null
+        "uuid" => "f9a2b033-e39b-4215-bfc9-c281f2436c7a"
+        "shipping_type" => "express"
+        */
+        $orders = Order::select(DB::raw('count(*) count, sum(total) total, DATE(created_at) date, YEAR(created_at) year, MONTH(created_at) month'));
+     
         switch($this->selected_filter) {
             case('date'):
 
@@ -57,41 +127,61 @@ class SalesReportGroup extends Component
                     $orders = $orders->whereDate('created_at', '=', $this->date_from);
 
                 }
-                
-
                 break;
  
             case('month'):
+
+                if($this->month_from && $this->year_from && $this->year_to && $this->year_from)
+                {
                     $orders = $orders
-                    //->whereMonth('created_at', '>=', $this->month_from)
-                    ->whereDate('created_at', '>=', $this->year_from.'-'.$this->month_from)
-                    ->whereDate('created_at', '<=', $this->year_to.'-'.$this->month_to);
-                    //date('m')
-                //$q->whereYear('created_at', '=', date('Y'));
-                    
+                    ->whereDate('created_at', '>=', date($this->year_from.'-'.$this->month_from.'-01').' 00:00:00')
+                    ->whereDate('created_at', '<=', date($this->year_to.'-'.$this->month_to.'-31').' 23:59:59');
+                }
+                else if($this->month_from && $this->year_from){
+                    $orders = $orders
+                    ->whereDate('created_at', '>=', date($this->year_from.'-'.$this->month_from.'-01').' 00:00:00')
+                    ->whereDate('created_at', '<=', date($this->year_from.'-'.$this->month_from.'-31').' 23:59:59');
+                }
                 break;
  
             case('year'):
-                    dd('i selected year');
+
+                if($this->year_from && $this->year_to)
+                {
+                    $orders = $orders
+                        ->whereYear('created_at', '>=', date($this->year_from))
+                        ->whereYear('created_at', '<=', date($this->year_to));
+                }
+                else if($this->year_from)
+                {
+                    $orders = $orders
+                    ->whereYear('created_at', '=', date($this->year_from));
+                }
             
                 break;
             default:
-                //Leave blank as of now
+                //Leave blank
             }
         if($this->group_by == 'date'){
-            $orders->groupBy(DB::raw("DATE(created_at)"));
+            $orders->groupBy('date');
         }  
-        if($this->group_by == 'month'){
-            $orders->groupBy(DB::raw("MONTH(created_at)"));
+        else if($this->group_by == 'month_year'){
+            $orders->groupBy('year', 'month');
         }
-        if($this->group_by == 'year'){
-            $orders->groupBy(DB::raw("YEAR(created_at)"));
+        else if($this->group_by == 'year'){
+            $orders->groupBy('year');
         } 
+        else {
+            redirect()->route('admin.reports');
+        }
            // whereDate('created_at', '=', $this->date) 
           //  ->where('status', '!=', 'cancelled') 
             //->paginate(10);
-        $orders = $orders->paginate(10);
-        //dd($orders);
+            
+        $this->genOrders = $orders->get();
+        $orders = $orders->get();
+
+        //$orders = $orders->paginate(10);
 
         return view('livewire.admin.sales-report-group', compact('orders'))->layout('layouts.admin');
     }
