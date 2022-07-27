5857162c15bdab12c5eb122d4818860e27af9f79
R M <31006518+rouprame@users.noreply.github.com>
2022-07-28 01:30:20 +0800

    Added activity log modal

---
+++ resources/views/livewire/admin/activity-log-modal.blade.php
@@ -0,0 +1,29 @@
+<x-modal>
+    <x-slot name="title">
+        Activity Log Details
+    </x-slot>
+    <x-slot name="content">
+        <div class="px-4 py-3 mb-8 bg-white rounded-lg">
+
+            Data:<br>
+            @foreach ($this->activity->properties['attributes'][0] as $key=>$item)
+                <b>{{ $key }}:</b> {{ $item}}<br>
+            @endforeach
+            <br><br>
+
+            
+            @if(str_contains($this->activity->description, 'Updated'))   
+            Updated data:<br>
+                @foreach ($this->activity->properties['old'][0] as $key=>$item)
+                    <b>{{ $key }}:</b> {{ $item }}<br>
+                @endforeach
+            @endif
+        </div>
+    </x-slot>
+
+    <x-slot name="buttons">
+        <button wire:click.prevent="$emit('closeModal')" class="w-full px-5 py-3 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray">
+            Close
+        </button>
+    </x-slot>
+</x-modal>
