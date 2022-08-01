5bafb70882011d20bb90d97335f5280710036ea3
R M <31006518+rouprame@users.noreply.github.com>
2022-08-01 12:33:23 +0800

    Added pending condition in invoice

--- resources/views/livewire/admin/order.blade.php
+++ resources/views/livewire/admin/order.blade.php
@@ -43,7 +43,7 @@
             <a href="{{ route('admin.order.details', $order->id ) }}" class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" aria-label="Edit">
                 View Order
             </a>
-                @if($order->status != 'cancelled')
+                @if($order->status != 'cancelled' && $order->status != 'pending')
                 <div class="my-2 px-2">
                     <a href="{{ route('admin.genOrderPDF', $order->uuid) }}" target="_blank" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
